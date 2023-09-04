b370fc2a7a4bb09c3f94b02d34863595ac6d9212
vasilyocheck <vasily.medvedev1986@gmail.com>
2023-09-04 08:38:30 +0300

    s1 w4 - callback training (rating + accordion + onOff)

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -1,48 +1,21 @@
 import React from 'react';
-import styled, {css} from "styled-components";
 
 type OnOffPropsType = {
     status: boolean
-    changeIsOnStatus?: () => void
+    changeIsOnStatus: () => void
 }
 
-export const OnOff: React.FC<OnOffPropsType> = (props: OnOffPropsType) => {
+export const OnOff: React.FC<OnOffPropsType> = (props) => {
+    function onClickHandler(){
+        props.changeIsOnStatus();
+    }
     return (
-
-        <StyledContainer>
-            <StyledOnButton status={props.status} onClick={props.changeIsOnStatus}>on</StyledOnButton>
-            <StyledOffButton status={props.status} onClick={props.changeIsOnStatus}>off</StyledOffButton>
-            <StyledCircle status={props.status}></StyledCircle>
-        </StyledContainer>
+        <div className='OnOff'>
+            <button style={{backgroundColor: props.status ? 'green' : ''}} onClick={onClickHandler}>on</button>
+            <button style={{backgroundColor: props.status ? '' : 'red'}} onClick={onClickHandler}>off</button>
+            <span className='circle' style={{backgroundColor: props.status ? 'green' : 'red'}}></span>
+        </div>
     );
 };
 
-const StyledContainer = styled.div`
-  display: flex;
-  gap: 20px;
-`;
-
-const StyledOnButton = styled.button<OnOffPropsType>`
-  ${props => props.status && css<{ status: boolean }>`
-    background-color: green;
-  `}
-`;
-const StyledOffButton = styled.button<OnOffPropsType>`
-  ${props => !props.status && css<{ status: boolean }>`
-    background-color: red;
-  `}
-`;
-
-const StyledCircle = styled.div<OnOffPropsType>`
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  //background-color: gray;
-  ${props => props.status && css<{ status: boolean }>`
-    background-color: green;
-  `}
-  ${props => !props.status && css<{ status: boolean }>`
-    background-color: red;
-  `}
-`;
 
